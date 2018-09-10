b001f79c26b68e7023015af47ad56d1f2a5f065d
Hiromasa Kaneko <31059982+hkaneko1985@users.noreply.github.com>
2018-09-10 15:58:38 +0900

    Add another optimal

--- Python/design_of_experiments.py
+++ Python/design_of_experiments.py
@@ -24,6 +24,8 @@ variables = {'variable1': [1, 2, 3, 4, 5],
              }
 # you can add 'variable4', 'variable5', ... after 'variable3' as well 
 
+optimal_type = 'D' # D, I
+number_of_random_searches = 1000
 # make all possible experiments
 all_experiments = np.array(variables['variable1'])
 all_experiments = np.reshape(all_experiments, (all_experiments.shape[0], 1))
@@ -37,16 +39,32 @@ all_experiments_df = pd.DataFrame(all_experiments)
 all_experiments_df.to_csv('all_experiments.csv', header=False, index=False)
 
 # select experiments
+np.random.seed(100) # fix random number seed
+experiment_indexes = np.arange(0, all_experiments.shape[0])
 autoscaled_all_experiments = (all_experiments - all_experiments.mean(axis=0)) / all_experiments.std(axis=0, ddof=1)
-for experiment_number in range(all_experiments.shape[0] - number_of_experiments):
-    determinants = []
-    autoscaled_all_experiments_tmp = autoscaled_all_experiments.copy()
-    for calc_determinant_number in range(all_experiments.shape[0]):
-        autoscaled_all_experiments_tmp = np.delete(autoscaled_all_experiments, calc_determinant_number, 0)
-        determinants.append(np.linalg.det(np.dot(autoscaled_all_experiments_tmp.T, autoscaled_all_experiments_tmp)))
-    selected_number = np.where(determinants == max(determinants))[0][0]
-    all_experiments = np.delete(all_experiments, selected_number, 0)
-    autoscaled_all_experiments = np.delete(autoscaled_all_experiments, selected_number, 0)
+for random_search_number in range(number_of_random_searches):
+    new_selected_experiment_numbers = np.random.randint(0, all_experiments.shape[0], number_of_experiments)
+    new_selected_experiments = all_experiments[new_selected_experiment_numbers, :]
+    autoscaled_new_selected_experiments = (new_selected_experiments - new_selected_experiments.mean(axis=0)) / new_selected_experiments.std(axis=0, ddof=1)
+    if optimal_type == 'D':
+        optimal_value = np.linalg.det(np.dot(autoscaled_new_selected_experiments.T, autoscaled_new_selected_experiments))
+    elif optimal_type == 'I':
+        optimal_value = sum(np.diag(autoscaled_new_selected_experiments.dot(np.linalg.inv(np.dot(autoscaled_new_selected_experiments.T, autoscaled_new_selected_experiments) / autoscaled_new_selected_experiments.shape[0])).dot(autoscaled_new_selected_experiments.T)))
+        
+    if random_search_number == 0:
+        best_optimal_value = optimal_value
+        selected_experiment_indexes = new_selected_experiment_numbers.copy()
+    else:
+        if optimal_type == 'D':
+            if best_optimal_value < optimal_value:
+                print(best_optimal_value)
+                selected_experiment_indexes = new_selected_experiment_numbers.copy()
+                best_optimal_value = optimal_value
+        elif optimal_type == 'I':
+            if best_optimal_value > optimal_value:
+                print(best_optimal_value)
+                selected_experiment_indexes = new_selected_experiment_numbers.copy()
+                best_optimal_value = optimal_value
 
-selected_experiments_df = pd.DataFrame(all_experiments)
+selected_experiments_df = pd.DataFrame(all_experiments[selected_experiment_indexes, :])
 selected_experiments_df.to_csv('selected_experiments.csv', header=False, index=False)
